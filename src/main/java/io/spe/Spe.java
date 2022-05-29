41fd1e5839b83ad9c9da7df398a1e6060bac143f
TheMode <themode@outlook.fr>
2022-05-29 08:00:05 +0200

    Cleanup

--- src/main/java/io/spe/Spe.java
+++ src/main/java/io/spe/Spe.java
@@ -12,6 +12,7 @@ import static org.bytedeco.libffi.global.ffi.*;
 import static org.bytedeco.llvm.global.LLVM.*;
 
 public final class Spe {
+    private static final String MAIN = "main";
     private static final Cleaner CLEANER = Cleaner.create();
     // a 'char *' used to retrieve error messages from LLVM
     private static final BytePointer error = new BytePointer();
@@ -27,9 +28,8 @@ public final class Spe {
     }
 
     public static <T> @NotNull T compile(String name, Class<T> type, Supplier<T> fallback) {
-        LLVMContextRef context = LLVMContextCreate();
-        LLVMOrcThreadSafeContextRef threadContext = LLVMOrcCreateNewThreadSafeContext();
-        LLVMModuleRef module = createModule(name, context);
+        var module = LLVMModuleCreateWithName(name);
+        fillModule(module);
 
         // Verify the module using LLVMVerifier
         if (LLVMVerifyModule(module, LLVMPrintMessageAction, error) != 0) {
@@ -43,9 +43,9 @@ public final class Spe {
         LLVMAddNewGVNPass(pm);
         LLVMAddCFGSimplificationPass(pm);
         LLVMRunPassManager(pm, module);
-        //LLVMDumpModule(module);
+        LLVMDumpModule(module);
 
-
+        LLVMOrcThreadSafeContextRef threadContext = LLVMOrcCreateNewThreadSafeContext();
         LLVMOrcThreadSafeModuleRef threadModule = LLVMOrcCreateNewThreadSafeModule(module, threadContext);
         // Execute using OrcJIT
         LLVMOrcLLJITRef jit = new LLVMOrcLLJITRef();
@@ -61,10 +61,10 @@ public final class Spe {
             throw new RuntimeException("Failed to add LLVM IR module");
         }
         final LongPointer res = new LongPointer(1);
-        if ((err = LLVMOrcLLJITLookup(jit, res, name)) != null) {
-            System.err.println("Failed to look up '" + name + "' symbol: " + LLVMGetErrorMessage(err));
+        if ((err = LLVMOrcLLJITLookup(jit, res, MAIN)) != null) {
+            System.err.println("Failed to look up '" + MAIN + "' symbol: " + LLVMGetErrorMessage(err));
             LLVMConsumeError(err);
-            throw new RuntimeException("Failed to look up '" + name + "' symbol");
+            throw new RuntimeException("Failed to look up '" + MAIN + "' symbol");
         }
 
         // Call the function
@@ -91,50 +91,50 @@ public final class Spe {
             LLVMOrcDisposeLLJIT(jit);
             LLVMDisposePassManager(pm);
             //LLVMDisposeBuilder(builder);
-            LLVMContextDispose(context);
         });
         return (T) factorial;
     }
 
-    private static LLVMModuleRef createModule(String name, LLVMContextRef context) {
-        LLVMModuleRef module = LLVMModuleCreateWithNameInContext(name, context);
-        LLVMBuilderRef builder = LLVMCreateBuilderInContext(context);
-        LLVMTypeRef i32Type = LLVMInt32TypeInContext(context);
-        LLVMTypeRef factorialType = LLVMFunctionType(i32Type, i32Type, /* argumentCount */ 1, /* isVariadic */ 0);
+    private static void fillModule(LLVMModuleRef module) {
+        LLVMBuilderRef builder = LLVMCreateBuilder();
+        try {
+            LLVMTypeRef i32Type = LLVMInt32Type();
+            LLVMTypeRef factorialType = LLVMFunctionType(i32Type, i32Type, /* argumentCount */ 1, /* isVariadic */ 0);
 
-        LLVMValueRef factorial = LLVMAddFunction(module, name, factorialType);
-        LLVMSetFunctionCallConv(factorial, LLVMCCallConv);
+            LLVMValueRef factorial = LLVMAddFunction(module, MAIN, factorialType);
+            LLVMSetFunctionCallConv(factorial, LLVMCCallConv);
 
-        LLVMValueRef n = LLVMGetParam(factorial, /* parameterIndex */0);
-        LLVMValueRef zero = LLVMConstInt(i32Type, 0, /* signExtend */ 0);
-        LLVMValueRef one = LLVMConstInt(i32Type, 1, /* signExtend */ 0);
-        LLVMBasicBlockRef entry = LLVMAppendBasicBlockInContext(context, factorial, "entry");
-        LLVMBasicBlockRef ifFalse = LLVMAppendBasicBlockInContext(context, factorial, "if_false");
-        LLVMBasicBlockRef exit = LLVMAppendBasicBlockInContext(context, factorial, "exit");
+            LLVMValueRef n = LLVMGetParam(factorial, 0);
+            LLVMValueRef zero = LLVMConstInt(i32Type, 0, 0);
+            LLVMValueRef one = LLVMConstInt(i32Type, 1, 0);
+            LLVMBasicBlockRef entry = LLVMAppendBasicBlock(factorial, "entry");
+            LLVMBasicBlockRef ifFalse = LLVMAppendBasicBlock(factorial, "if_false");
+            LLVMBasicBlockRef exit = LLVMAppendBasicBlock(factorial, "exit");
 
-        LLVMPositionBuilderAtEnd(builder, entry);
-        LLVMValueRef condition = LLVMBuildICmp(builder, LLVMIntEQ, n, zero, "condition = n == 0");
-        LLVMBuildCondBr(builder, condition, exit, ifFalse);
+            LLVMPositionBuilderAtEnd(builder, entry);
+            LLVMValueRef condition = LLVMBuildICmp(builder, LLVMIntEQ, n, zero, "condition = n == 0");
+            LLVMBuildCondBr(builder, condition, exit, ifFalse);
 
-        LLVMPositionBuilderAtEnd(builder, ifFalse);
-        LLVMValueRef nMinusOne = LLVMBuildSub(builder, n, one, "nMinusOne = n - 1");
-        PointerPointer<Pointer> arguments = new PointerPointer<>(1)
-                .put(0, nMinusOne);
-        LLVMValueRef factorialResult = LLVMBuildCall2(builder, factorialType, factorial, arguments, 1, "factorialResult = factorial(nMinusOne)");
-        LLVMValueRef resultIfFalse = LLVMBuildMul(builder, n, factorialResult, "resultIfFalse = n * factorialResult");
-        LLVMBuildBr(builder, exit);
+            LLVMPositionBuilderAtEnd(builder, ifFalse);
+            LLVMValueRef nMinusOne = LLVMBuildSub(builder, n, one, "nMinusOne = n - 1");
+            PointerPointer<Pointer> arguments = new PointerPointer<>(1)
+                    .put(0, nMinusOne);
+            LLVMValueRef factorialResult = LLVMBuildCall2(builder, factorialType, factorial, arguments, 1, "factorialResult = factorial(nMinusOne)");
+            LLVMValueRef resultIfFalse = LLVMBuildMul(builder, n, factorialResult, "resultIfFalse = n * factorialResult");
+            LLVMBuildBr(builder, exit);
 
-        LLVMPositionBuilderAtEnd(builder, exit);
-        LLVMValueRef phi = LLVMBuildPhi(builder, i32Type, "result");
-        PointerPointer<Pointer> phiValues = new PointerPointer<>(2)
-                .put(0, one)
-                .put(1, resultIfFalse);
-        PointerPointer<Pointer> phiBlocks = new PointerPointer<>(2)
-                .put(0, entry)
-                .put(1, ifFalse);
-        LLVMAddIncoming(phi, phiValues, phiBlocks, /* pairCount */ 2);
-        LLVMBuildRet(builder, phi);
-
-        return module;
+            LLVMPositionBuilderAtEnd(builder, exit);
+            LLVMValueRef phi = LLVMBuildPhi(builder, i32Type, "result");
+            PointerPointer<Pointer> phiValues = new PointerPointer<>(2)
+                    .put(0, one)
+                    .put(1, resultIfFalse);
+            PointerPointer<Pointer> phiBlocks = new PointerPointer<>(2)
+                    .put(0, entry)
+                    .put(1, ifFalse);
+            LLVMAddIncoming(phi, phiValues, phiBlocks, 2);
+            LLVMBuildRet(builder, phi);
+        } finally {
+            LLVMDisposeBuilder(builder);
+        }
     }
 }
