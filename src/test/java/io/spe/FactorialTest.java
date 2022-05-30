dea2cfb511ff8fd24d110e9923f523b2d194663f
TheMode <themode@outlook.fr>
2022-05-30 05:43:13 +0200

    Only use inner impl

--- src/test/java/io/spe/FactorialTest.java
+++ src/test/java/io/spe/FactorialTest.java
@@ -1,15 +1,15 @@
 package io.spe;
 
 import io.spe.api.SpeTest;
-import io.spe.type.FactorialImpl;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpeTest
 public class FactorialTest {
+
     @Test
-    public void separateFile() {
+    public void test() {
         Factorial function = Spe.compile("factorial", Factorial.class, FactorialImpl.class);
         assertEquals(1, function.factorial(1));
         assertEquals(2, function.factorial(2));
@@ -24,23 +24,7 @@ public class FactorialTest {
         assertEquals(39916800, function.factorial(11));
     }
 
-    @Test
-    public void innerImpl() {
-        Factorial function = Spe.compile("factorial", Factorial.class, InnerFactorial.class);
-        assertEquals(1, function.factorial(1));
-        assertEquals(2, function.factorial(2));
-        assertEquals(6, function.factorial(3));
-        assertEquals(24, function.factorial(4));
-        assertEquals(120, function.factorial(5));
-        assertEquals(720, function.factorial(6));
-        assertEquals(5040, function.factorial(7));
-        assertEquals(40320, function.factorial(8));
-        assertEquals(362880, function.factorial(9));
-        assertEquals(3628800, function.factorial(10));
-        assertEquals(39916800, function.factorial(11));
-    }
-
-    private static final class InnerFactorial implements Factorial {
+    private static final class FactorialImpl implements Factorial {
         @Override
         public int factorial(int n) {
             if (n == 0) return 1;
