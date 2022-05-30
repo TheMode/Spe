c3b4c6a79f0c938aefa312abe46cb946063e91a8
TheMode <themode@outlook.fr>
2022-05-30 05:34:00 +0200

    WIP jmh

---
+++ jmh-benchmarks/src/main/java/io/spe/FactorialBench.java
@@ -0,0 +1,40 @@
+package io.spe;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+@Warmup(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(3)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class FactorialBench {
+
+    @Param({"false", "true"})
+    public boolean nativeCode = true;
+
+    Factorial factorial;
+
+    @Setup
+    public void setup() {
+        this.factorial = nativeCode ? Spe.compile("factorial", Factorial.class, new FactorialImpl()) : new FactorialImpl();
+    }
+
+    @Benchmark
+    public void factorial(Blackhole blackhole) {
+        for (int i = 0; i < 10; i++) {
+            blackhole.consume(1);
+        }
+    }
+
+    public static final class FactorialImpl implements Factorial {
+        @Override
+        public int factorial(int n) {
+            if (n == 0) return 1;
+            else return (n * factorial(n - 1));
+        }
+    }
+}
