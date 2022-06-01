276974222d6a7e9743b0d550604a30e7b090db75
TheMode <themode@outlook.fr>
2022-06-01 18:18:19 +0200

    More maps in SpeSignature

--- src/main/java/io/spe/SpeSignature.java
+++ src/main/java/io/spe/SpeSignature.java
@@ -10,11 +10,24 @@ import static java.util.Map.entry;
 import static org.objectweb.asm.Opcodes.*;
 
 final class SpeSignature {
-    private static final Map<Class<?>, MemoryLayout> BASICS = Map.ofEntries(
+    private static final Map<Class<?>, MemoryLayout> TYPE_TO_LAYOUT = Map.ofEntries(
             entry(boolean.class, JAVA_BOOLEAN), entry(byte.class, JAVA_BYTE),
             entry(char.class, JAVA_CHAR), entry(short.class, JAVA_SHORT),
             entry(int.class, JAVA_INT), entry(long.class, JAVA_LONG),
             entry(float.class, JAVA_FLOAT), entry(double.class, JAVA_DOUBLE));
+
+    private static final Map<Class<?>, Integer> TYPE_TO_LOAD = Map.ofEntries(
+            entry(boolean.class, ILOAD), entry(byte.class, ILOAD),
+            entry(char.class, ILOAD), entry(short.class, ILOAD),
+            entry(int.class, ILOAD), entry(long.class, LLOAD),
+            entry(float.class, FLOAD), entry(double.class, DLOAD));
+
+    private static final Map<Class<?>, Integer> TYPE_TO_RETURN = Map.ofEntries(
+            entry(void.class, RETURN),
+            entry(boolean.class, IRETURN), entry(byte.class, IRETURN),
+            entry(char.class, IRETURN), entry(short.class, IRETURN),
+            entry(int.class, IRETURN), entry(long.class, LRETURN),
+            entry(float.class, FRETURN), entry(double.class, DRETURN));
 
     public static FunctionDescriptor descriptor(Class<?> result, Class<?>[] parameterTypes) {
         final MemoryLayout[] params = Arrays.stream(parameterTypes).map(SpeSignature::toLayout).toArray(MemoryLayout[]::new);
@@ -27,27 +40,20 @@ final class SpeSignature {
     }
 
     private static MemoryLayout toLayout(Class<?> type) {
-        final MemoryLayout layout = BASICS.get(type);
+        final MemoryLayout layout = TYPE_TO_LAYOUT.get(type);
         if (layout == null) throw new IllegalArgumentException("Unsupported type: " + type);
         return layout;
     }
 
     static int loadOpcode(Class<?> type) {
-        if (type == boolean.class || type == byte.class || type == char.class || type == short.class || type == int.class)
-            return ILOAD;
-        if (type == long.class) return LLOAD;
-        if (type == float.class) return FLOAD;
-        if (type == double.class) return DLOAD;
-        throw new IllegalArgumentException("Unsupported layout: " + type);
+        final Integer opcode = TYPE_TO_LOAD.get(type);
+        if (opcode == null) throw new IllegalArgumentException("Unsupported type: " + type);
+        return opcode;
     }
 
     static int returnOpcode(Class<?> type) {
-        if (type == void.class) return RETURN;
-        if (type == boolean.class || type == byte.class || type == char.class || type == short.class || type == int.class)
-            return IRETURN;
-        if (type == long.class) return LRETURN;
-        if (type == float.class) return FRETURN;
-        if (type == double.class) return DRETURN;
-        throw new IllegalArgumentException("Unsupported layout: " + type);
+        final Integer opcode = TYPE_TO_RETURN.get(type);
+        if (opcode == null) throw new IllegalArgumentException("Unsupported type: " + type);
+        return opcode;
     }
 }
