542443557795222a4b7a365f6f005fe18dd958db
TheMode <themode@outlook.fr>
2022-06-03 13:50:30 +0200

    Add Chain

--- compiler/src/test/java/io/spe/basics/IfTest.java
+++ compiler/src/test/java/io/spe/basics/IfTest.java
@@ -17,6 +17,15 @@ public class IfTest {
         assertEquals(false, function.test(3));
     }
 
+    @Test
+    public void chainTest() {
+        Chain function = Spe.compileAndCreate(Chain.class, Chain.Impl.class);
+        assertEquals(20, function.get(0));
+        assertEquals(5, function.get(1));
+        assertEquals(10, function.get(2));
+        assertEquals(15, function.get(3));
+    }
+
     @FunctionalInterface
     public interface IsEven {
         boolean test(int n);
@@ -25,6 +34,21 @@ public class IfTest {
             @Override
             public boolean test(int n) {
                 return n % 2 == 0;
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface Chain {
+        int get(int n);
+
+        class Impl implements Chain {
+            @Override
+            public int get(int n) {
+                if (n == 1) return 5;
+                else if (n == 2) return 10;
+                else if (n == 3) return 15;
+                else return 20;
             }
         }
     }
